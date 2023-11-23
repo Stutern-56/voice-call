c6cce15cbdb72107318e75fbef0f81c06f1062b1
Ekpa-N <ekpa.ntan@gmail.com>
2023-11-23 15:36:51 +0100

    side bar first build

---
+++ client/voice-call/constants/menuList.js
@@ -0,0 +1,20 @@
+const menu = [
+    {
+        name: "Dashboard",
+        img: "./images/home-03.svg"
+    },
+    {
+        name: "Call History",
+        img: "./images/material-symbols_call-sharp.svg"
+    },
+    {
+        name: "Notification",
+        img: "./images/notification-01.svg"
+    },
+    {
+        name: "Settings",
+        img: "./images/setting-01.svg"
+    },
+]
+
+export default menu 
